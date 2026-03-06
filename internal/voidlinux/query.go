bb519f0c8acce17b8de1bdde46dae9fc31406e4d
skewb1k <skewb1kunix@gmail.com>
2026-03-07 01:11:44 +0300

    Fix Void Linux querier to exact match

--- internal/voidlinux/query.go
+++ internal/voidlinux/query.go
@@ -1,29 +1,18 @@
 package voidlinux
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
-type responseBody struct {
-	Data []json.RawMessage `json:"data"`
-}
-
 func Query(query string) (bool, error) {
-	url := fmt.Sprintf("https://xq-api.voidlinux.org/v1/query/x86_64?q=%s", query)
+	url := fmt.Sprintf("https://api.github.com/repos/void-linux/void-packages/contents/srcpkgs/%s", query)
 	resp, err := http.Get(url)
 	if err != nil {
 		return false, err
 	}
 	defer resp.Body.Close()
 
-	var r responseBody
-	err = json.NewDecoder(resp.Body).Decode(&r)
-	if err != nil {
-		return false, err
-	}
-
-	ok := len(r.Data) != 0
+	ok := resp.StatusCode == http.StatusOK
 	return ok, nil
 }
