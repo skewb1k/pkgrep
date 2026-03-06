d5ec8c49fcd41d577cf5d7f768404c5abec0a424
skewb1k <skewb1kunix@gmail.com>
2026-03-06 21:39:37 +0300

    Implement query CLI argument parsing

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s QUERY\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	if flag.NArg() != 1 {
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	query := flag.Arg(0)
+	fmt.Printf("Query: %s\n", query)
 }
