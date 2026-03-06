707804e9813df45cc4e433406ca498f25b3e53e6
skewb1k <skewb1kunix@gmail.com>
2026-03-06 23:01:29 +0300

    Fix Arch Linux querier to search in all sub-repos

--- internal/archlinux/query.go
+++ internal/archlinux/query.go
@@ -1,18 +1,29 @@
 package archlinux
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type responseBody struct {
+	Results []json.RawMessage `json:"results"`
+}
+
 func Query(query string) (bool, error) {
-	url := fmt.Sprintf("https://archlinux.org/packages/core/x86_64/%s/json/", query)
+	url := fmt.Sprintf("https://archlinux.org/packages/search/json/?name=%s", query)
 	resp, err := http.Get(url)
 	if err != nil {
 		return false, err
 	}
 	defer resp.Body.Close()
 
-	ok := resp.StatusCode == http.StatusOK
+	var r responseBody
+	err = json.NewDecoder(resp.Body).Decode(&r)
+	if err != nil {
+		return false, err
+	}
+
+	ok := len(r.Results) != 0
 	return ok, nil
 }
