cec311f7978d9876ffd622234c3dacfd6e450b25
skewb1k <skewb1kunix@gmail.com>
2026-03-07 02:00:47 +0300

    Fix Alpine Linux querier to search in all sub-repos

--- internal/alpine/query.go
+++ internal/alpine/query.go
@@ -1,18 +1,30 @@
 package alpine
 
 import (
+	"bufio"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func Query(query string) (bool, error) {
-	url := fmt.Sprintf("https://pkgs.alpinelinux.org/package/edge/testing/x86_64/%s", query)
+	url := fmt.Sprintf("https://pkgs.alpinelinux.org/packages?branch=edge&name=%s", query)
 	resp, err := http.Get(url)
 	if err != nil {
 		return false, err
 	}
 	defer resp.Body.Close()
 
-	ok := resp.StatusCode == http.StatusOK
-	return ok, nil
+	scanner := bufio.NewScanner(resp.Body)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, "No matching packages found...") {
+			return false, nil
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return false, err
+	}
+
+	return true, nil
 }
