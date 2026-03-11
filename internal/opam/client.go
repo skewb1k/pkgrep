46e3d8d3f3e4d9865d65d8d241e8e0e22af9c17f
Nick Asmodeus <nikitakitsunedev@gmail.com>
2026-03-11 12:30:48 +0300

    Update internal/opam/client.go
    Co-authored-by: skewb1k <skewb1kunix@gmail.com>

--- internal/opam/client.go
+++ internal/opam/client.go
@@ -13,6 +13,5 @@ type Client struct {
 
 func (c Client) Query(query string) (bool, error) {
 	url := fmt.Sprintf("https://opam.ocaml.org/packages/%s/", query)
-	ok, err := httputil.GetCheckOK(c.HTTPClient, url)
-	return ok, err
+	return httputil.GetCheckOK(c.HTTPClient, url)
 }
