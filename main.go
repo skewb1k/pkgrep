skewb1k/pkgrep

commit 8fcaa9f28d957bcea788cd2a3618935453eb3bce

Refactor main.go

main.go
@@ -10,7 +10,6 @@ import (
 	"strings"
 	"sync"
 	"time"
-	"unicode"
 
 	"github.com/skewb1k/pkgrep/internal/alpine"
 	"github.com/skewb1k/pkgrep/internal/aosc"
@@ -103,21 +102,6 @@ var repos = []Repository{
 	{"Void", voidlinux.Client{httpClient}},
 }
 
-type Result struct {
-	Name  string
-	Found bool
-}
-
-// safeURLSegment checks whether a string can be safely placed in URL segment.
-func safeURLSegment(q string) error {
-	for _, r := range q {
-		if !unicode.IsLetter(r) && !unicode.IsDigit(r) && r != '-' && r != '.' && r != '_' && r != '~' {
-			return fmt.Errorf("disallowed character %q", r)
-		}
-	}
-	return nil
-}
-
 type repoList []string
 
 func (rl *repoList) String() string {
@@ -139,6 +123,14 @@ var flagExclude repoList
 func init() {
 	flag.Var(&flagInclude, "include", "search in specified repositories only")
 	flag.Var(&flagExclude, "exclude", "skip specified repositories")
+}
+
+// Checks if repository name is explicitly excluded or not included via flags.
+func shouldSkipRepository(repoName string) bool {
+	nameLower := strings.ToLower(repoName)
+	excluded := slices.Contains(flagExclude, nameLower)
+	included := len(flagInclude) > 0 && slices.Contains(flagInclude, nameLower)
+	return excluded || !included
 }
 
 func main() {
@@ -169,13 +161,15 @@ func main() {
 		log.Fatal("invalid query: ", err)
 	}
 
-	var wg sync.WaitGroup
+	type Result struct {
+		Name  string
+		Found bool
+	}
 	results := make(chan Result)
 
+	var wg sync.WaitGroup
 	for _, repo := range repos {
-		nameLower := strings.ToLower(repo.Name)
-		if slices.Contains(flagExclude, nameLower) ||
-			(len(flagInclude) > 0 && !slices.Contains(flagInclude, nameLower)) {
+		if shouldSkipRepository(repo.Name) {
 			continue
 		}
 		wg.Add(1)

safeurl.go
@@ -1,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+// safeURLSegment checks whether a string can be safely placed in URL segment.
+func safeURLSegment(s string) error {
+	for _, r := range s {
+		if !unicode.IsLetter(r) &&
+			!unicode.IsDigit(r) &&
+			r != '-' &&
+			r != '.' &&
+			r != '_' &&
+			r != '~' {
+			return fmt.Errorf("disallowed character in URL %q", r)
+		}
+	}
+	return nil
+}

commit c4c8439a6b48b3e75843291395c12be8e5d3a8a6

Fix no repos being included without flags
Regression from 8fcaa9f28d957bcea788cd2a3618935453eb3bce

main.go
@@ -129,7 +129,7 @@ func init() {
 func shouldSkipRepository(repoName string) bool {
 	nameLower := strings.ToLower(repoName)
 	excluded := slices.Contains(flagExclude, nameLower)
-	included := len(flagInclude) > 0 && slices.Contains(flagInclude, nameLower)
+	included := len(flagInclude) == 0 || slices.Contains(flagInclude, nameLower)
 	return excluded || !included
 }
 